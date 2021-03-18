95d40a4bf26ebfdeaca041fcfaf4acd2d2eb3c0f
KyrosKrane <kyros@kyros.info>
2021-03-18 18:53:57 -0500

    Correct display format for output

--- ChatLinkTest.lua
+++ ChatLinkTest.lua
@@ -15,9 +15,9 @@ local addonName, CLT = ...
 
 function CLT:HandleClick(CallbackID, DisplayText, Data)
 	DEFAULT_CHAT_FRAME:AddMessage("HandleClick called with parameters:")
-	DEFAULT_CHAT_FRAME:AddMessage("    CallbackID = >>", CallbackID, "<<")
-	DEFAULT_CHAT_FRAME:AddMessage("    DisplayText = >>", DisplayText, "<<")
-	DEFAULT_CHAT_FRAME:AddMessage("    Data = >>", Data, "<<")
+	DEFAULT_CHAT_FRAME:AddMessage("    CallbackID = >>" .. CallbackID .. "<<")
+	DEFAULT_CHAT_FRAME:AddMessage("    DisplayText = >>" .. DisplayText .. "<<")
+	DEFAULT_CHAT_FRAME:AddMessage("    Data = >>" .. Data .. "<<")
 end
 
 
