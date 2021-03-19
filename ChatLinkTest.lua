e2a9f9abbd43f8cc33eeb7ed79e701993f7857b2
KyrosKrane <kyros@kyros.info>
2021-03-18 19:32:01 -0500

    Create a testing framework and two test cases

--- ChatLinkTest.lua
+++ ChatLinkTest.lua
@@ -10,8 +10,20 @@
 
 -- This add-on file creates some test cases for the ChatLink-1.0 library
 
-local addonName, CLT = ...
 
+--#########################################
+--# Setup
+--#########################################
+
+local addonName, CLT = ...
+local ChatLink = LibStub("ChatLink-1.0")
+
+--#########################################
+--# Click handling
+--#########################################
+
+-- Note that since we don't actually do anything with the data, one function is enough for all test cases.
+-- We only create a second to ensure callbacks are calling the right function.
 
 function CLT:HandleClick(CallbackID, DisplayText, Data)
 	DEFAULT_CHAT_FRAME:AddMessage("HandleClick called with parameters:")
@@ -20,19 +32,46 @@ function CLT:HandleClick(CallbackID, DisplayText, Data)
 	DEFAULT_CHAT_FRAME:AddMessage("    Data = >>" .. Data .. "<<")
 end
 
-
-
-local ChatLink = LibStub("ChatLink-1.0")
-local DisplayText = "click this link"
-local Link, CallbackID = ChatLink:CreateChatLink(DisplayText)
-if CallbackID then
-	ChatLink.RegisterCallback(CLT, CallbackID, "HandleClick")
-else
-	DEFAULT_CHAT_FRAME:AddMessage("CreateChatLink() FAILED. CallbackID nil or false.")
+function CLT:SecondHandleClick(One, Two, Three)
+	DEFAULT_CHAT_FRAME:AddMessage("SecondHandleClick called with parameters:")
+	DEFAULT_CHAT_FRAME:AddMessage("    CallbackID = >>" .. One .. "<<")
+	DEFAULT_CHAT_FRAME:AddMessage("    DisplayText = >>" .. Two .. "<<")
+	DEFAULT_CHAT_FRAME:AddMessage("    Data = >>" .. Three .. "<<")
 end
 
-if Link then
-	DEFAULT_CHAT_FRAME:AddMessage("This is a test of the ChatLink library. You can " .. Link .. " to run the test.")
-else
-	DEFAULT_CHAT_FRAME:AddMessage("CreateChatLink() FAILED. Link nil or false.")
+
+--#########################################
+--# Test case link generator
+--#########################################
+
+-- This function generates a test case link.
+-- The first parameter is the callback function name to handle the click.
+-- The next five parameters are used to create the link.
+
+local function GenerateTC(Handler, DisplayText, Data, SkipFormat, CallbackID, PublicMessageID)
+	-- create link
+	local Link, ReturnedCallbackID = ChatLink:CreateChatLink(DisplayText)
+
+	-- verify successful creation
+	if not ReturnedCallbackID then
+		DEFAULT_CHAT_FRAME:AddMessage("CreateChatLink() FAILED for " .. DisplayText .. ". CallbackID nil or false.")
+	end
+	if not Link then
+		DEFAULT_CHAT_FRAME:AddMessage("CreateChatLink() FAILED for " .. DisplayText .. ". Link nil or false.")
+	end
+	if not ReturnedCallbackID or not Link then return end
+
+	-- Register to handle the click event
+	ChatLink.RegisterCallback(CLT, ReturnedCallbackID, Handler)
+
+	-- Display the test case
+	DEFAULT_CHAT_FRAME:AddMessage("Click this link for " .. Link .. ".")
 end
+
+
+--#########################################
+--# Test cases
+--#########################################
+
+GenerateTC("HandleClick", "Test case 1: Basic link with display text only")
+GenerateTC("SecondHandleClick", "Test case 2: Alternate click handler")
